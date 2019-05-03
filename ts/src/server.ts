8248a559505ca05f9390e288c060b2942db595f0
Joshua Richardson <joshua@webcoders.com.au>
2019-05-03 15:50:46 +0930

    Re-add socket options

--- ts/src/server.ts
+++ ts/src/server.ts
@@ -6,8 +6,10 @@ import 'reflect-metadata';
 import { getAppAsync } from './app';
 import { assertConfigsAreValid } from './assertions';
 import { configs } from './production_configs';
-import { NetworkIdToProvider, NetworkSpecificSettings } from './types';
+import { NetworkIdToProvider, NetworkSpecificSettings, ServerMode } from './types';
 import { utils } from './utils';
+
+import * as fs from 'fs';
 
 (async () => {
     assertConfigsAreValid(configs);
@@ -25,7 +27,22 @@ import { utils } from './utils';
 
     const app = await getAppAsync(networkIdToProvider, configs);
 
-    app.listen(configs.HTTP_PORT, () => {
-        utils.log(`Coordinator SERVER API (HTTP) listening on port ${configs.HTTP_PORT}!`);
-    });
+    if (configs.SERVER_MODE === ServerMode.HttpPort) {
+        app.listen(configs.HTTP_PORT, () => {
+            utils.log(`Coordinator SERVER API (HTTP) listening on port ${configs.HTTP_PORT}!`);
+        });
+    }
+    else if (configs.SERVER_MODE === ServerMode.UnixSocket) {
+        try {
+            fs.unlinkSync(configs.SOCKET_FILE);
+        } catch (err) {}
+
+        app.listen(configs.SOCKET_FILE, () => {
+            utils.log(`Coordinator SERVER API (HTTP) listening on socket ${configs.SOCKET_FILE}!`);
+
+            try {
+                fs.chmodSync(configs.SOCKET_FILE, '755');
+            } catch (err) {}
+        });
+    }
 })().catch(utils.log);
